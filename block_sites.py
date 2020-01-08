93bf88512922c171ceeaa9fb914162c24a71b544
Ondrej Tichy <ondrejtichy07@gmail.com>
2020-01-08 23:58:08 +0100

    first

---
+++ block_sites.py
@@ -0,0 +1,32 @@
+# Run this script as root
+import time
+from datetime import datetime as dt
+
+hosts_path = "/etc/hosts"
+redirect = "127.0.0.1"
+
+website_list = ["www.facebook.com","facebook.com",
+	"www.twitter.com","twitter.com",
+	"www.youtube.com","youtube.com"]
+
+while True:
+	if dt(dt.now().year, dt.now().month, dt.now().day,8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,16): 
+		print("BLOCKING")
+		with open(hosts_path, 'r+') as file:
+			content = file.read()
+			for website in website_list:
+				if website in content:
+					pass
+				else:
+					file.write(redirect + " " + website + "\n")
+	else:
+		with open(hosts_path, 'r+') as file:
+			content=file.readlines()
+			file.seek(0)
+			for line in content:
+				if not any(website in line for website in website_list):
+					file.write(line)
+			file.truncate()
+
+		print("NOT BLOCKING")
+	time.sleep(5)
